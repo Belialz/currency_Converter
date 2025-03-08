53754f2bfd14978261ae268efc54e1c8c2787711
Ilya Murashka <belbelialz888@gmail.com>
2025-03-08 01:30:49 +0100

    Fix bug with saved 'To Currency'

--- src/popup.js
+++ src/popup.js
@@ -12,26 +12,7 @@ chrome.storage.local.get('baseCurrency', (data) => {
   }
 });
 chrome.storage.local.get('currencyTo', (data) => {
-  if (data.baseCurrency) {
-    document.getElementById('currency-to-convert').value = data.baseCurrency;
-  }
-});
-
-// Функция для отображения Toast-уведомления
-function showToastNotification(numbers, converted) {
-  const resultMessage = `${numbers} USD ≈ ${converted} PLN`;
-  
-  // Показываем уведомление
-  toastr.success(resultMessage, "Currency Converter");
-}
-
-// Слушаем сообщения от background.js
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "conversionResult") {
-    // Получаем результат из сообщения
-    const { numbers, converted } = message;
-    
-    // Показываем уведомление с результатом
-    showToastNotification(numbers, converted);
+  if (data.currencyTo) {
+    document.getElementById('currency-to-convert').value = data.currencyTo;
   }
 });
